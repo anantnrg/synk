df7d064b473f049ec4b7eb8c931ada9b70a8bffc
Anant Narayan <anantnrg@gmail.com>
2024-02-22 17:23:39 +0530

    Wipeout freya and start working on custom renderer

--- crates/ui/src/lib.rs
+++ crates/ui/src/lib.rs
@@ -1,62 +1,10 @@
-pub mod colors;
-pub mod editor;
-pub mod separator;
-pub mod sidebar;
-pub mod statusbar;
-pub mod tab_bar;
+pub mod window;
 
-use colors::Colors;
-use freya::prelude::*;
-use synk_core::document::Document;
+pub struct Ui {}
 
-use crate::{
-    editor::{Editor, EditorConfig},
-    separator::{HorizontalSeparator, VerticalSeparator},
-    sidebar::Sidebar,
-    statusbar::Statusbar,
-    tab_bar::TabBar,
-};
-
-#[allow(non_snake_case)]
-pub fn SynkUI() -> Element {
-    let colors = Colors::new();
-    let sidebar_width = use_signal(|| 300_isize);
-    let document = Document::new(
-        "fn main() {\n    let x = \"Hello!\\n\";\n    println!(\"{x}\");\n}".to_string(),
-    );
-
-    rsx! {
-        rect {
-            width: "100%",
-            height: "100%",
-            direction: "horizontal",
-            background: "{colors.background}",
-            color: "{colors.foreground}",
-            cross_align: "center",
-            font_family: "JetBrains Mono",
-            overflow: "clip",
-            Sidebar { width: sidebar_width, colors: colors.clone().sidebar }
-            VerticalSeparator {
-                colors: colors.separator.clone(),
-                interactive: true,
-                callback: sidebar_width,
-                reverse: false
-            }
-            rect { width: "calc(100% - {sidebar_width})", height: "100%", direction: "vertical",
-                TabBar { colors: colors.clone().tab_bar }
-                HorizontalSeparator { colors: colors.separator.clone(), interactive: false, reverse: false }
-                Editor {
-                    colors: colors.clone(),
-                    config: EditorConfig {
-    line_height: 36.0,
-    font_family: "JetBrains Mono",
-    font_size: 16.0,
-    document,
-}
-                }
-                HorizontalSeparator { colors: colors.separator.clone(), interactive: false, reverse: false }
-                Statusbar { colors: colors.statusbar }
-            }
-        }
+impl Ui {
+    pub fn new() -> Ui {
+        Ui {}
     }
+    pub fn run(&mut self) {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,34 @@
-use freya::prelude::*;
-use winit::dpi::LogicalSize;
+use ui::Ui;
 
 #[allow(dead_code)]
 const APP_ID: &'static str = "synk";
 const JETBRAINS_MONO: &[u8] = include_bytes!("../resources/fonts/jetbrains_mono.ttf");
 
 fn main() {
-    let builder_hook: WindowBuilderHook = Box::new(|mut builder| {
-        builder = builder
-            .with_inner_size(LogicalSize::new(1400, 800))
-            .with_active(true)
-            .with_visible(true)
-            .with_resizable(true)
-            .with_title("Synk Editor");
+    // let builder_hook: WindowBuilderHook = Box::new(|mut builder| {
+    //     builder = builder
+    //         .with_inner_size(LogicalSize::new(1400, 800))
+    //         .with_active(true)
+    //         .with_visible(true)
+    //         .with_resizable(true)
+    //         .with_title("Synk Editor");
 
-        #[allow(dead_code)]
-        #[cfg(feature = "wayland")]
-        {
-            use winit::platform::wayland::WindowBuilderExtWayland as _;
-            builder = builder.with_name(APP_ID, "");
-        }
+    //     #[allow(dead_code)]
+    //     #[cfg(feature = "wayland")]
+    //     {
+    //         use winit::platform::wayland::WindowBuilderExtWayland as _;
+    //         builder = builder.with_name(APP_ID, "");
+    //     }
 
-        #[allow(dead_code)]
-        #[cfg(feature = "x11")]
-        {
-            use winit::platform::x11::WindowBuilderExtX11 as _;
-            builder = builder.with_name(APP_ID, "");
-        }
-        builder
-    });
+    //     #[allow(dead_code)]
+    //     #[cfg(feature = "x11")]
+    //     {
+    //         use winit::platform::x11::WindowBuilderExtX11 as _;
+    //         builder = builder.with_name(APP_ID, "");
+    //     }
+    //     builder
+    // });
 
-    launch_cfg(
-        ui::SynkUI,
-        LaunchConfig::<()>::builder()
-            .with_font("JetBrains Mono", JETBRAINS_MONO)
-            .with_window_builder(builder_hook)
-            .build(),
-    );
+    let mut ui = Ui::new();
+    ui.run();
 }
