ddc2e530a4e09d95b152da0506ba724e1d18dbf0
Anant Narayan <anantnrg@gmail.com>
2024-02-22 22:59:18 +0530

    remove commented code

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,6 @@ const APP_ID: &'static str = "synk";
 const JETBRAINS_MONO: &[u8] = include_bytes!("../resources/fonts/jetbrains_mono.ttf");
 
 fn main() {
-    // let builder_hook: WindowBuilderHook = Box::new(|mut builder| {
-    //     builder = builder
-    //         .with_inner_size(LogicalSize::new(1400, 800))
-    //         .with_active(true)
-    //         .with_visible(true)
-    //         .with_resizable(true)
-    //         .with_title("Synk Editor");
-
-    //     #[allow(dead_code)]
-    //     #[cfg(feature = "wayland")]
-    //     {
-    //         use winit::platform::wayland::WindowBuilderExtWayland as _;
-    //         builder = builder.with_name(APP_ID, "");
-    //     }
-
-    //     #[allow(dead_code)]
-    //     #[cfg(feature = "x11")]
-    //     {
-    //         use winit::platform::x11::WindowBuilderExtX11 as _;
-    //         builder = builder.with_name(APP_ID, "");
-    //     }
-    //     builder
-    // });
-
     let mut ui = Ui::new();
     ui.run();
 }
