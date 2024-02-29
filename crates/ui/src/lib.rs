7d0424cb5db9b3500d1b32c27871ec875adf0196
Anant Narayan <anantnrg@gmail.com>
2024-02-29 11:05:36 +0530

    Add Ui struct

--- crates/ui/src/lib.rs
+++ crates/ui/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub struct Ui {}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Ui {
+    pub fn new() -> Self {
+        Ui {}
     }
+    pub fn run(&mut self) {}
 }
